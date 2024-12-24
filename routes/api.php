d8b80d34bfb2ff0bc6ec8bae33994adf469ae8df
MED SALEM GHALASS <msghalas@gmail.com>
2024-12-24 07:50:47 +0000

    post crud

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
-Route::get('/', function () {
-    return 'API';
-});
+Route::apiResource('posts', PostController::class);
